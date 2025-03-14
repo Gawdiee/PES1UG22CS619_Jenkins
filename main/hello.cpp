5fb98cc37c7faac174f631fdc3cdf0cea6b9dd4a
Sudeep Hosur <159408755+Gawdiee@users.noreply.github.com>
2025-03-14 11:44:36 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "NAME: Sudeep Hosur";
+  cout << "SRN: PES1UG22CS619"
   return 0;
 }
